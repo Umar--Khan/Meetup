c87dae750c191df52d4d7b8ff1ae574fb8df74f1
oli-gra <oliver.grant@gmail.com>
2019-04-15 22:40:36 +0100

    users done, tags all but user_id event_id error

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,22 +1,52 @@
 class TagsController < ApplicationController
+
   def new
+    flash[:data] ? @tag = Tag.new(flash[:data]) : @tag = Tag.new
   end
 
   def create
+    tag = Tag.new(get_params)
+    if tag.save
+      redirect_to tag_path tag
+    else
+      flash[:errors] = tag.errors.full_messages
+      flash[:data] = tag
+      redirect_to new_tag_url
+    end
   end
 
   def update
+    tag = Tag.find(params[:id])
+    if tag.update(get_params)
+      redirect_to tag_path tag
+    else
+      flash[:errors] = tag.errors.full_messages
+      redirect_to tags_edit
+    end
   end
 
   def edit
+    @tag = Tag.find(params[:id])
   end
 
   def destroy
+    @tag = tag.find(params[:id])
+    @tag.delete
+    redirect_to tags_path
   end
 
   def index
+    @tags = Tag.all
   end
 
   def show
+    @tag = Tag.find(params[:id])
   end
+
+  private
+
+  def get_params
+    params.require(:tag).permit(:main_tag, :sub_tag_01, :sub_tag_02)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,52 @@
 class UsersController < ApplicationController
+
   def new
+    flash[:data] ? @user = User.new(flash[:data]) : @user = User.new
   end
 
   def create
-  end
-
-  def update
-  end
-
-  def edit
+    user = User.new(get_params)
+    if user.save
+      redirect_to user_path user
+    else
+      flash[:errors] = user.errors.full_messages
+      flash[:data] = user
+      redirect_to users_new
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.delete
+    redirect_to users_path
   end
 
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(get_params)
+      redirect_to user_path user
+    else
+      flash[:errors] = user.errors.full_messages
+      redirect_to users_edit
+    end
+  end
+
+  private
+
+  def get_params
+    params.require(:user).permit(:name, :email, :dob, :loc)
+  end
+
 end
