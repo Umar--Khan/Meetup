9e32d6b700ae2412e5b109751bc6dddd7b9caab9
Umar--Khan <umarkhandev@gmail.com>
2019-04-17 23:38:34 +0100

    messages_working

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,10 +2,13 @@ class MessagesController < ApplicationController
 
     def show
         @message = Message.find(params[:id])
-        @messages = Message.all.where(id: @message.id)
+        @messages = Message.all.where(event_id: @message.event_id)
     end
     
     def content
-        redirect_to message_path(@message)
+        message = Message.find(params[:id])
+        string = "#{current_user.name}: " + params[:message][:content]
+        content = Message.create(title: message.title, content: string, event_id: message.event_id)
+        redirect_to message_path(message)
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events
-  resources :messages, only: [:show]
+  resources :messages
   resources :imgs, only: [:show]
   resources :users, :tags
   root to: 'pages#home'
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   post "/logged", to: "sessions#logged"
   delete "/logout", to: 'sessions#logout'
   post '/events/:id/attend', to: 'events#attend', as: "events_attend"
-  post '/messages/:id'
+  patch '/messages/:id/content', to: 'messages#content', as: "content"
 end
