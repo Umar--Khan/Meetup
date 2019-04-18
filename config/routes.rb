b60008bdf8adc9af4bc4fe749e36a3a3b1055886
oli-gra <oliver.grant@gmail.com>
2019-04-18 08:46:46 +0100

    to_start_styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'ratings/index'
+  get 'ratings/show'
+  get 'ratings/new'
+  get 'ratings/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events
   resources :messages, only: [:show]
@@ -9,5 +13,5 @@ Rails.application.routes.draw do
   post "/logged", to: "sessions#logged"
   delete "/logout", to: 'sessions#logout'
   post '/events/:id/attend', to: 'events#attend', as: "events_attend"
-  post '/messages/:id'
+  post '/messages/:id', to:  'messages#content'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_17_085633) do
+ActiveRecord::Schema.define(version: 2019_04_18_053317) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2019_04_17_085633) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "event_id"
     t.index ["user_id"], name: "index_ratings_on_user_id"
   end
 
