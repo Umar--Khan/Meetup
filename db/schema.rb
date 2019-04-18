d2bbc180ebec916073da3d18ece1e6fb1da2c9cf
Umar--Khan <umarkhandev@gmail.com>
2019-04-18 09:59:15 +0100

    schema_fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_04_18_000830) do
-
+ActiveRecord::Schema.define(version: 2019_04_18_053317) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
